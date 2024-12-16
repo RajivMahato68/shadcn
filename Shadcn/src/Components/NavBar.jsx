de79fa2463d03aa40a0768db21a4596630021457
rajipmahato68 <rajipmahato68@gmail.com>
2024-12-16 17:02:37 +0545

    Implement dialogbox

--- Shadcn/src/Components/NavBar.jsx
+++ Shadcn/src/Components/NavBar.jsx
@@ -1,7 +1,9 @@
 import { ModeToggle } from "./ModeToggle";
+// import { Link } from "react-router-dom";
 function NavBar() {
   return (
     <div className="flex mb-8 mx-auto border-b border-gray-200 py-4 px-8 dark:border-gray-700 ">
+      {/* <Link to="/">hello</Link> */}
       <ModeToggle />
     </div>
   );
--- Shadcn/src/Components/pages.jsx
+++ Shadcn/src/Components/pages.jsx
@@ -1,4 +1,5 @@
 import BoldHeading from "./BoldHeading";
+import { ShadModal } from "./shadModal";
 
 function Pages() {
   return (
@@ -7,6 +8,9 @@ function Pages() {
       <h2 className="scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight first:mt-0">
         The People of the Kingdom
       </h2>
+      <div className="py-8">
+        <ShadModal />
+      </div>
     </div>
   );
 }
